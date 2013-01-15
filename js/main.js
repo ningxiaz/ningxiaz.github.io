78599a5808c32460f8e00fb2bdacc2b0e5eb69bc
Ningxia Zhang <thmmrs2298@gmail.com>
2013-01-15 01:10:17 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	resize_section();
+
+	$(window).on("resize", function(){ 
+		resize_section();
+	});
+
+	$('.works li').hover(function(){
+		$(this).find('.hover').fadeIn(100);
+	}, function(){
+		$(this).find('.hover').fadeOut(100);
+	});
+});
+
+function resize_section(){
+	var height = $(window).height();
+	var width = $(window).width();
+
+	//set the height of intro section and place the teaser in the right place
+	$('#intro').css('height', height);
+	$('.teaser').css('top', height - 42);
+
+	//resize the background images
+	if($(window).width()>1000){
+		$('.bg').css('background-size', '100% auto');
+	}
+	else{
+		$('.bg').css('background-size', 'auto 100%');
+	}
+
+	//resize the background image of intro section
+	if(height*14.0/9 < width){
+		$('.bg').css('background-size', '100% auto');
+	}
+}
